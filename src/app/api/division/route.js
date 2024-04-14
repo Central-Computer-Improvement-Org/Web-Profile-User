476af31d7e6c749128c79e94d93809076520a041
muamarzidan <codeofomiru11@gmail.com>
2024-04-14 14:20:46 +0700

    RBLD-18 | Fix Images & implement mockup, Fix Responsive | Home

---
+++ src/app/api/division/route.js
@@ -0,0 +1,59 @@
+export async function GET(request) {
+    const responseData = {
+        code: 200,
+        status: "SUCCESS",
+        recordsTotal: 6,
+        data: [
+            {
+                id: "DVS-458163-1",
+                name: "Web Development",
+                description: "Divisi yang berfokus pada pembelajaran pengembangan website terbaru dengan memperhatikan beberapa struktur didalamnya.",
+                logo_uri: "assets/uploads/division/logo-divisi-web.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            },
+            {
+                id: "DVS-458163-2",
+                name: "Design",
+                description: "Divisi yang berfokus mempelajari UI/UX melalui beberapa tahapan didalamnya sehingga divisi design memiliki tujuan atau memberikan output berupa sebuah desain produk sebaik mungkin.",
+                logo_uri: "assets/uploads/division/logo-divisi-design.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            },
+            {
+                id: "DVS-458163-3",
+                name: "Games and Gadget",
+                description: "Divisi Games and Gadget merupakan divisi yang berfokus dalam pengembangan games dan juga kegiatan- kegiatan lainnya yang berhubungan dengan video games.",
+                logo_uri: "assets/uploads/division/logo-divisi-gng.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            },
+            {
+                id: "DVS-458163-4",
+                name: "Network",
+                description: "Divisi yang berfokus pada pengaturan dan manajemen jaringan komputer sehingga mencapai hasil yang optimal.",
+                logo_uri: "assets/uploads/division/logo-divisi-network.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            },
+            {
+                id: "DVS-458163-5",
+                name: "Media Management",
+                description: "Divisi ini yang bergerak di bidang media dari CCI, seperti content creator, content planner, design content, disb. Divisi ini berfokus mempelajari bagaimana mengatur, memanagement dan mendesain sebuah media.",
+                logo_uri: "assets/uploads/division/logo-divisi-mm.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            },
+            {
+                id: "DVS-458163-6",
+                name: "Data Research",
+                description: "Divisi yang berfokus pada proses dimana mengumpulkan, mengukur, dan menganalisis data dari berbagai sumber untuk mendapatkan wawasan atau pemahaman terhadap suatu hal.",
+                logo_uri: "assets/uploads/division/logo-divisi-ds.png",
+                createdAt: "2013-02-26 21:28:37.261134+01:00",
+                updatedAt: "2013-02-26 21:28:37.261134+01:00",
+            }
+        ],
+        error: null,
+    };
+    return new Response(JSON.stringify(responseData));
+}
--- src/components/Home/newsSecondSlider.js
+++ src/components/Home/newsSecondSlider.js
@@ -1,46 +1,64 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
+import request from "@/app/utils/request";
 import styles from "@/components/Home/homeComponent.module.css";
 
-const newsImageOne = "assets/images/home/news-thumbnail-1.png";
-const newsImageTwo = "assets/images/home/news-thumbnail-2.png";
+const NewsSecondSlider = () => {
+  const router = useRouter();
+  const [newsData, setNewsData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const dataCard = [
-  {
-    image: newsImageOne,
-    title: "AI dalam Kehidupan Sehari-Hari",
-    date: "Mar 12, 2024",
-  },
-  {
-    image: newsImageTwo,
-    title: "Lorem ipsum dolor sit amet dan",
-    date: "Mar 12, 2024",
-  },
-  {
-    image: newsImageOne,
-    title: "Energi blockchain dan masa depan",
-    date: "Mar 12, 2024",
-  },
-  {
-    image: newsImageTwo,
-    title:
-      "Peran AI dalam dunia pendidikan bagi manusia dwdwd wdwdw wdwd ewdwd dw",
-    date: "Mar 12, 2024",
-  },
-  {
-    image: newsImageOne,
-    title: "AI untuk keberlanjutan lingkungan hidup",
-    date: "Mar 12, 2024",
-  },
-];
+  useEffect(() => {
+    request
+      .get("/news")
+      .then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          const formattedData = response.data.data.map((item) => {
+            const createdAt = new Date(item.createdAt);
+            const updatedAt = new Date(item.updatedAt);
+            return {
+              ...item,
+              date: `${getMonthName(createdAt.getMonth())} ${createdAt.getDate()}, ${createdAt.getFullYear()}`,  // get from createdAt
+              // updatedAt: `${getMonthName(updatedAt.getMonth())} ${updatedAt.getDate()}, ${updatedAt.getFullYear()}` // get from updatedAt
+            };
+          });
+          setNewsData(formattedData);
+        } else {
+          console.error(JSON.stringify(response.errors));
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
 
-const SecondSlider = () => {
+  const getMonthName = (monthIndex) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return months[monthIndex];
+  };
+
   return (
     <Swiper
       slidesPerView={1.4}
@@ -59,34 +77,35 @@ const SecondSlider = () => {
       modules={[Pagination]}
       className={`w-full`}
     >
-      {dataCard.map((data, index) => (
-        <SwiperSlide key={index}>
-          <div onClick={() => router.push("/news")} className="w-full">
-            <Image
-              width={220}
-              height={150}
-              src={data.image}
-              alt="News Thumbnail Central Computer Improvment"
-              responsive="true"
-              className="w-full h-auto rounded-t-xl object-cover"
-            />
-            <div
-              className={`h-[100px] max-h-[100px] flex flex-col justify-between p-2 bg-white rounded-b-xl ${styles.newsCardBorder}`}
-            >
-              <h2
-                className={`font-semibold text-[14px] leading-5 sm:leading-0 overflow-hidden text-bluePallete-800 ${styles.newsCardTitle}`}
+      {Array.isArray(newsData) &&
+        newsData.map((data, index) => (
+          <SwiperSlide key={index}>
+            <div onClick={() => router.push("/news")} className="w-full">
+              <Image
+                width={220}
+                height={150}
+                src={data.media_uri}
+                alt="News Thumbnail Central Computer Improvment"
+                responsive="true"
+                className="w-full h-auto rounded-t-xl object-cover"
+              />
+              <div
+                className={`h-[100px] max-h-[100px] flex flex-col justify-between p-2 bg-white rounded-b-xl ${styles.newsCardBorder}`}
               >
-                {data.title}
-              </h2>
-              <p className="text-[10px] font-medium text-gray-500">
-                {data.date}
-              </p>
+                <h2
+                  className={`font-semibold text-[14px] leading-5 sm:leading-0 overflow-hidden text-bluePallete-800 ${styles.newsCardTitle}`}
+                >
+                  {data.title}
+                </h2>
+                <p className="text-[10px] font-medium text-gray-500">
+                  {data.date}
+                </p>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
+          </SwiperSlide>
+        ))}
     </Swiper>
   );
 };
 
-export default SecondSlider;
+export default NewsSecondSlider;
