24285091227528636ca332e2432c543205a52a60
aryafirmansyah2 <aryafirmansyah200404@example.com>
2024-05-04 23:41:30 +0700

    RBLD-37 | Projects - Slicing Projects UI | Projects

--- src/app/division/page.js
+++ src/app/division/page.js
@@ -1,11 +1,7 @@
-"use client";
-import React, { Suspense } from "react";
-import DetailDvision from "@/components/Division/DetailDivision";
+'use client';
+import React from 'react';
+import DetailDvision from '@/components/Division/DetailDivision';
 
 export default function DivisionPage() {
-  return (
-    <Suspense fallback={<div className="text-center text-[32px] text-bluePallete-800">Loading...</div>}>
-      <DetailDvision />
-    </Suspense>
-  );
+  return <DetailDvision />;
 }
