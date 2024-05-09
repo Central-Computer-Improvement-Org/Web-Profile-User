b94b2f27cbb7a2b5531f05cf9c4dd3df2428d782
kevinirawand <kevinid07@gmail.com>
2024-05-09 21:16:18 +0700

    fix image not found

--- src/components/imageNotFound.js
+++ src/components/imageNotFound.js
@@ -2,16 +2,16 @@
 import Image from "next/image";
 
 const NotFound = ({ className = "w-[50px] h-[50px]" }) => {
-  return (
-    <Image
-      src="/assets/icon/notfound.jpg"
-      alt="Not Found Icon"
-      width={500}
-      height={500}
-      responsive="true"
-      className={`${className}`}
-    />
-  );
+   return (
+      <Image
+         src="assets/icon/notfound.jpg"
+         alt="Not Found Icon"
+         width={500}
+         height={500}
+         responsive="true"
+         className={`${className}`}
+      />
+   );
 };
 
 export default NotFound;
