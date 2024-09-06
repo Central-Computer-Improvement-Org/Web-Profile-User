e091ea4e359fbec5fe50006b6350418c3a3a0a4b
kevinirawand <kevinid07@gmail.com>
2024-09-06 17:02:06 +0700

    checkpoint

--- src/components/credit/crewSlider.js
+++ src/components/credit/crewSlider.js
@@ -7,12 +7,26 @@ import 'swiper/css/grid';
 import 'swiper/css/navigation';
 
 import { host } from '../host';
-import { ImageNotFound } from '../imageNotFound';
 import CardCreditProfile from './cardCreditProfile';
 import NotFound from '../imageNotFound';
 import styles from './credit.module.css';
+import ReactCardFlip from "react-card-flip";
+import TeksNotFound from "@/components/teksNotFound";
+import Image from "next/image";
 
 // const LIMITER = 4;
+
+const desktopColorPattern = [
+    "#152E51",
+    "#11A950",
+    "#11A950",
+    "#152E51",
+    "#152E51",
+    "#11A950",
+    "#11A950",
+];
+
+
 export default function CrewSlider({
   crewDatas = [],
   color = 'bg-bluePallete-300',
@@ -21,6 +35,10 @@ export default function CrewSlider({
   const [page, setPage] = useState(1)
   const [splittedDatas, setSplittedDatas] = useState([])
   const [datas, setDatas] = useState([])
+    const [flipPosition, setFlipPosition] = useState(null);
+    const [autoFlipCount, setAutoFlipCount] = useState(0);
+    const [colorPattern, setColorPattern] = useState(desktopColorPattern);
+
   const iconArrow = (
     <svg
       className="xl:w-[55px] md:w-[40px] "
@@ -36,43 +54,22 @@ export default function CrewSlider({
       />
     </svg>
   );
-  
-  // const [page, setPage] = useState(1);
-  // const [totalPages, setTotalPages] = useState(0);
-  // const [crewDatas2, setCrewDatas2] = useState([]);
 
+  console.info(crewDatas)
 
-  // useEffect(() => {
-  //   if (crewDatas.length > LIMITER) {
-  //     // const totalPages = Math.ceil(crewDatas.length / LIMITER);
-  //     // console.log("total", totalPages);
-      
-  //     setTotalPages(Math.ceil(crewDatas.length / LIMITER));
-      
-  //     const start = (page - 1) * LIMITER;
-  //     const end = start + LIMITER;
-  //     setCrewDatas2(crewDatas.slice(start, end));
-  //     console.log("start", start);
-  //     console.log("end", end);
-  //     // setCrewDatas2(crewDatas.slice(0, 2));
-  //   }
-  // }, [crewDatas, page]);
-  
-  // console.log("totalpages:", totalPages);  
-  // console.log("crewDatas2:", crewDatas2);
-  // console.log("pages:", page);
+  const handleNext = () => {
+      page > 2 ? setPage(1) : setPage(page + 1)
+  };
 
-  
+    function onClickFlipCard(index) {
+        setFlipPosition(flipPosition === index ? null : index);
+    }
 
-//   if (!crewDatas || crewDatas.length === 0) {
-//     return (
-//        <div className="w-full h-auto flex justify-center items-center mt-[50px]">
-//           <ImageNotFound className="w-[90px] h-[78px] sm:w-[130px] sm:h-[108px] lg:w-[170px] lg:h-[148px] object-cover" />
-//        </div>
-//     );
-//  }
+    const getCardBackgroundColor = (index) => {
+        const patternIndex = index % colorPattern.length;
+        return colorPattern[patternIndex];
+    };
 
-  // const sisaSlide
     useEffect(() => {
         const splited = [
             crewDatas?.slice(0, 4),
@@ -82,11 +79,6 @@ export default function CrewSlider({
 
     }, [crewDatas]);
 
-
-  const handleNext = () => {
-      page > 2 ? setPage(1) : setPage(page + 1)
-  };
-
     useEffect(() => {
         console.info(splittedDatas)
 
@@ -94,103 +86,99 @@ export default function CrewSlider({
     }, [splittedDatas, page]);
 
   return crewDatas.length ? (
-    <div className="relative">
-      <Swiper
-        onReachEnd={(e) => {
-          const sisaSlide = crewDatas.length - e.activeIndex; // Menghitung sisa data berdasarkan index aktif
-          console.log("Sisa Slide:", sisaSlide);
-          
-          if (sisaSlide > 0 && sisaSlide < e.params.slidesPerView) {
-            // Jika sisa slide lebih kecil dari slidesPerView
-            e.params.slidesPerView = sisaSlide; // Ubah slidesPerView sesuai dengan sisa slide
-            e.update(); // Update swiper setelah merubah konfigurasi
-          }
-        }}
-        grid={
-          size.width > 425
-            ? {
-              rows: 2,
-              fill: 'row',
-            }
-            : {
-              rows: 1,
-              fill: 'row',
-            }
-        }
-        // slidesPerGroup={3}
-        // slidesPerView="auto"
-        slidesPerView={
-          size.width > 425
-            ? 2
-            : 1
-        }
-        setWrapperSize={true}
-        loopPreventsSliding={true}
-        spaceBetween={20}
-        breakpoints={{
-          425: {
-            slidePerView: 1,
-            spaceBetween: 20,
-            slidesPerColumnFill: 'row',
-            direction: 'horizontal',
-            centerInsufficientSlides:true,
-          },
-          640: {
-            slidePerView: 2,
-            spaceBetween: 40,
-            slidesPerColumnFill: 'row',
-            direction: 'horizontal',
-            centerInsufficientSlides:true,
-          }
-        }}
-        // slidesPerColumn: 2,
-        // slidesPerColumnFill='row'
-        centerInsufficientSlides={true}
-        direction='horizontal'
-        navigation={{
-          nextEl: '.next',
-        }}
-        loop={false}
-        pagination={
-          size.width >= 768
-            ? false
-            : {
-              el: '.bullets-container',
-              type: 'bullets',
-              bulletClass: 'swiper-custom-bullet',
-              bulletActiveClass: 'swiper-custom-bullet-active',
-              clickable: true,
-              renderBullet: function (index, className) {
-                return '<span className="' + className + '"></span>';
-              },
-            }
-        }
-        modules={[Grid, Navigation, Pagination]}
-        className="mySwiper"
-      >
-        {datas?.length &&
-          datas?.map((data, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <div className="flex items-center justify-center">
-                  <CardCreditProfile
-                    color={color}
-                    image={`${host}${data?.profileUri}`}
-                    jobdes={`${data.role?.name}`}
-                    name={data?.name}
-                  />
-                </div>
-              </SwiperSlide>
-            );
-          })}
-      </Swiper>
-      <div className="bullets-container"></div>
-      <div className={`${styles.test} absolute z-10 top-0 bottom-0 lg:flex items-center justify-center hidden`}>
-        <button onClick={handleNext} className="bg-bluePallete-500 text-transparent rounded-full xl:w-[100px] w-[80px] xl:h-[100px] h-[80px] flex items-center justify-center next">
-          {iconArrow}
-        </button>
-      </div>
-    </div>
+      <>
+          <div className="w-full h-auto flex flex-wrap justify-around items-center mt-[21px]">
+              {datas?.map((crew, index) => (
+                  <div key={crew.id} className="mt-[10px] sm:mt-[54px]">
+                      <ReactCardFlip
+                          key={crew.id}
+                          isFlipped={flipPosition === index}
+                          flipDirection="vertical"
+                      >
+                          {/* Card bagian depan */}
+                          <div
+                              className={`relative w-[300px] sm:w-[517px] h-[65px] sm:h-full max-w-[517px] max-h-[198px] rounded-[15px] sm:rounded-[30px] overflow-hidden cursor-pointer ${styles.dvisionEventCardContainer}`}
+                              onClick={() => onClickFlipCard(index)}
+                          >
+                              <div
+                                  className="absolute inset-0 rounded-[15px] sm:rounded-[30px] flex justify-center items-center"
+                                  style={{
+                                      backgroundColor: `${getCardBackgroundColor(index)}80`,
+                                  }}
+                              >
+                                  {crew?.name ? (
+                                      <p className="font-black text-[14px] sm:text-[40px] [text-shadow:_0px_6px_7px_rgb(0_0_0_/_40%)] text-[#F5FBF9]">
+                                          {crew.name}
+                                      </p>
+                                  ) : (
+                                      <TeksNotFound className="font-black text-[14px] sm:text-[40px] [text-shadow:_0px_6px_7px_rgb(0_0_0_/_40%)] text-[#F5FBF9]"></TeksNotFound>
+                                  )}
+                              </div>
+                              {crew?.mediaUri ? (
+                                  <Image
+                                      src={`${host}${crew.mediaUri}`}
+                                      alt={[crew.title + " Central Computer Improvement"]}
+                                      width={517}
+                                      height={198}
+                                      responsive="true"
+                                      loading="lazy"
+                                      className="object-cover w-full h-full"
+                                  />
+                              ) : (
+                                  <NotFound className="object-cover w-full h-full" />
+                              )}
+                          </div>
+                          {/* Card bagian belakang */}
+                          <div
+                              className={`w-[300px] sm:w-[517px] h-[65px] sm:h-[198px] max-w-[517px] max-h-[198px] flex flex-col justify-start items-left space-y-0 sm:space-y-1 px-[26px] sm:px-[60px] py-[5px] sm:py-[30px] rounded-[15px] sm:rounded-[30px] cursor-pointer ${styles.dvisionEventCardContainer}`}
+                              style={{
+                                  backgroundColor: getCardBackgroundColor(index),
+                              }}
+                              onClick={() => onClickFlipCard(index)}
+                          >
+                              {crew?.name ? (
+                                  <h1 className="font-black text-[14px] sm:text-[24px] text-white">
+                                      {crew.name}
+                                  </h1>
+                              ) : (
+                                  <TeksNotFound className="font-black text-[14px] sm:text-[24px] text-white"></TeksNotFound>
+                              )}
+                              {crew?.name ? (
+                                  <p className="text-[8px] sm:text-[16px] font-medium text-white">
+                                      {crew.description}
+                                  </p>
+                              ) : (
+                                  <TeksNotFound className="text-[8px] sm:text-[16px] font-medium text-white"></TeksNotFound>
+                              )}
+                          </div>
+                      </ReactCardFlip>
+                  </div>
+              ))}
+          </div>
+          <div className="flex items-center justify-center w-full">
+              <button
+                  className={`${styles.crewButton} w-full flex justify-center hover:opacity-75 mt-[20px] sm:mt-[54px] text-white`}
+                  onClick={handleNext}
+              >
+                  <svg
+                      width="100"
+                      height="100"
+                      viewBox="0 0 100 100"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-[50px] h-[50px] sm:w-[100px] sm:h-[100px]"
+                  >
+                      <circle cx="50" cy="50" r="50" fill="#265290" />
+                      <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M48.9661 33.2082C49.9729 32.2014 51.6053 32.2014 52.6121 33.2082L68.0808 48.677C69.0876 49.6838 69.0876 51.3162 68.0808 52.323L52.6121 67.7918C51.6053 68.7986 49.9729 68.7986 48.9661 67.7918C47.9592 66.7849 47.9592 65.1526 48.9661 64.1457L60.0337 53.0781H32.7422C31.3183 53.0781 30.1641 51.9239 30.1641 50.5C30.1641 49.0761 31.3183 47.9219 32.7422 47.9219H60.0337L48.9661 36.8543C47.9592 35.8474 47.9592 34.2151 48.9661 33.2082Z"
+                          fill="white"
+                      />
+                  </svg>
+              </button >
+          </div>
+      </>
   ) : (
     <div className="flex justify-center w-[100%]">
       <NotFound className="flex justify-center w-[200px]" />
